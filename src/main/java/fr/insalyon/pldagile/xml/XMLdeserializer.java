46a9419a1bce687688e4339b03fb3232d976d75e
bdeleglise <benoit.deleglise@outlook.fr>
2021-10-15 10:48:28 +0200

    XML deserializes intersections and segments

--- src/main/java/fr/insalyon/pldagile/xml/XMLdeserializer.java
+++ src/main/java/fr/insalyon/pldagile/xml/XMLdeserializer.java
@@ -12,8 +12,58 @@ import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
+import fr.insalyon.pldagile.model.Coordinates;
+import fr.insalyon.pldagile.model.Intersection;
+import fr.insalyon.pldagile.model.Segment;
 
 
 public class XMLdeserializer {
 
+
+    /**
+     * TODO : javadoc
+     * @param elt
+     * @return
+     * @throws ExceptionXML
+     */
+    private static Intersection createIntersection(Element elt) throws  ExceptionXML{
+        double latitude = Double.parseDouble(elt.getAttribute("latitude"));
+        double longitude = Double.parseDouble(elt.getAttribute("longitude"));
+        Coordinates coordinates = new Coordinates(latitude,longitude);
+        if(coordinates == null){
+            throw new ExceptionXML("Error when reading file: Wrong Coordinates Format");
+        }
+        Integer id = Integer.parseInt(elt.getAttribute("id"));
+        if(id < 0) {
+            throw new ExceptionXML("Error when reading file: id must be null or positive");
+        }
+        return new Intersection(id, coordinates);
+    }
+
+    /**
+     * TODO : javadoc
+     * @param elt
+     * @return
+     * @throws ExceptionXML
+     */
+    private static Segment createSegment(Element elt) throws  ExceptionXML{
+        Integer destinationID = Integer.parseInt(elt.getAttribute("destination"));
+        Integer originID = Integer.parseInt(elt.getAttribute("origin"));
+        if(destinationID < 0){
+            throw new ExceptionXML("Error when reading file: Wrong destination ID");
+        }
+        if(originID < 0) {
+            throw new ExceptionXML("Error when reading file: Wrong origin ID");
+        }
+        String name = elt.getAttribute("name");
+        Double length = Double.parseDouble(elt.getAttribute("length"));
+        if(length <= 0){
+            throw  new ExceptionXML("Error when reading file: Wrong length");
+        }
+        //TODO : import plan to have the list of intersection
+        return new Segment(name,length,null, null);
+
+
+
+    }
 }
