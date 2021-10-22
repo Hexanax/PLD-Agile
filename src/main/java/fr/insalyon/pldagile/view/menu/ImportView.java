93f4652328c4e2fa385281a2600b3cea8988763f
Alexis <alexismrx@hotmail.fr>
2021-10-22 08:39:01 +0200

    Correction

--- src/main/java/fr/insalyon/pldagile/view/menu/ImportView.java
+++ src/main/java/fr/insalyon/pldagile/view/menu/ImportView.java
@@ -24,7 +24,7 @@ public class ImportView extends Region {
     private Label importMapLabel;
     private Label importPickupLabel;
 
-    public ImportView() {
+    public ImportView () {
         //TODO Move outside of constructor with function calls
         GridPane gridPane = new GridPane();
         gridPane.setAlignment(Pos.CENTER);
@@ -34,21 +34,9 @@ public class ImportView extends Region {
 
         Label titleLabel = new Label("Import");
         titleLabel.setFont(Font.font("Arial", FontWeight.BOLD, 24));
-<<<<<<< Updated upstream
-<<<<<<< HEAD:src/main/java/fr/insalyon/pldagile/view/menu/ImportView.java
         gridPane.add(titleLabel, 0,0,2,1);
-=======
-        maingp.add(titleLabel, 0,0,3,1);
->>>>>>> ihm:src/main/java/fr/insalyon/pldagile/ui/maps/ImportView.java
-=======
-<<<<<<< Updated upstream
-        gridPane.add(titleLabel, 0,0,2,1);
-=======
-        gridPane.add(titleLabel, 0, 0, 2, 1);
->>>>>>> Stashed changes
->>>>>>> Stashed changes
         GridPane.setHalignment(titleLabel, HPos.CENTER);
-        GridPane.setMargin(titleLabel, new Insets(0, 0, 10, 0));
+        GridPane.setMargin(titleLabel, new Insets(0, 0,10,0));
 
         importMapButton = new Button("Import map");
         importPickupButton = new Button("Import Requests");
@@ -59,24 +47,6 @@ public class ImportView extends Region {
         importPickupLabel = new Label("No file imported yet");
         gridPane.add(importMapLabel, 0, 2, 1, 1);
         gridPane.add(importPickupLabel, 1, 2, 1, 1);
-<<<<<<< Updated upstream
-=======
-        maingp.add(importMapButton, 0, 1, 1, 1);
-        maingp.add(importPickupButton, 2, 1, 1, 1);
-
-        importMapLabel = new Label("No file imported yet");
-        importPickupLabel = new Label("No file imported yet");
-        maingp.add(importMapLabel, 0, 2, 1, 1);
-        maingp.add(importPickupLabel, 2, 2, 1, 1);
->>>>>>> ihm:src/main/java/fr/insalyon/pldagile/ui/maps/ImportView.java
-=======
-<<<<<<< Updated upstream
-=======
-
-        importMapLabel = new Label("No file imported yet");
-        importPickupLabel = new Label("No file imported yet");
->>>>>>> Stashed changes
->>>>>>> Stashed changes
 
 
         computeButton = new Button("Compute tour");
@@ -85,91 +55,47 @@ public class ImportView extends Region {
         computeButton.setPrefWidth(100);
         gridPane.add(computeButton, 0, 3, 2, 1);
         GridPane.setHalignment(computeButton, HPos.CENTER);
-        GridPane.setMargin(computeButton, new Insets(10, 0, 0, 0));
+        GridPane.setMargin(computeButton, new Insets(10, 0,0,0));
 
         //TODO Start TSP resolution algorithm
         //TODO Move in controller for handling and action setup
         computeButton.setOnAction(event -> {
             System.out.println("Computing tour...");
             PickyApplication.renderTour();
-<<<<<<< Updated upstream
-=======
-        maingp.add(computeButton, 0, 3, 3, 1);
-        GridPane.setHalignment(computeButton, HPos.CENTER);
-        GridPane.setMargin(computeButton, new Insets(10, 0,0,0));
-
-        this.getChildren().add(maingp);
-
-        importMapButton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                try {
-                    XMLDeserializer.load(PickyApplication.getCityMap());
-                    PickyApplication.updateCityMap();
-                    importMapLabel.setText("");
-                } catch(Exception e) {
-                    e.printStackTrace();
-                }
-            }
->>>>>>> ihm:src/main/java/fr/insalyon/pldagile/ui/maps/ImportView.java
-=======
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
         });
 
         this.getChildren().add(gridPane);
-=======
-            GridPane.setHalignment(computeButton, HPos.CENTER);
-            GridPane.setMargin(computeButton, new Insets(10, 0, 0, 0));
-            
-            importMapButton.setOnAction(new EventHandler<ActionEvent>() {
-                @Override
-                public void handle(ActionEvent event) {
-                    try {
-                        XMLDeserializer.load(PickyApplication.getCityMap());
-                        PickyApplication.updateCityMap();
-                        importMapLabel.setText("");
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-            });
 
-            this.getChildren().add(gridPane);
-
-            //TODO Move in controller for handling and action setup
-            importMapButton.setOnAction(event -> {
-                try {
-                    File importFile = XMLFileOpener.getInstance().open(FileChooseOption.READ);
-                    PickyApplication.emptyCityMap();
-                    PickyApplication.emptyPlanningRequest(); //New loaded map means we reset the requests too
-                    XMLDeserializer.load(PickyApplication.getCityMap(), importFile); //TODO Handle empty city & requests as load file success callback to prevent the "cancel" and invalid format from resetting
-                } catch (Exception e) {
-                    e.printStackTrace();
-                } finally {
-                    PickyApplication.clearMap();
-                    PickyApplication.renderMapAndRequests();
-                }
-            });
->>>>>>> Stashed changes
-
-            //TODO Move in controller for handling and action setup
-            importPickupButton.setOnAction(event -> {
-                try {
-                    File importFile = XMLFileOpener.getInstance().open(FileChooseOption.READ);
-                    PickyApplication.emptyPlanningRequest();
-                    XMLDeserializer.load(PickyApplication.getPlanningRequest(), PickyApplication.getCityMap(), importFile); //TODO Handle emptyPlanningRequest as load file success callback to prevent the "cancel" and invalid format from resetting
-                } catch (Exception e) {
-                    e.printStackTrace();
-                } finally {
-                    PickyApplication.clearMap();
-                    PickyApplication.renderMapAndRequests();
-                }
-            });
-
+        //TODO Move in controller for handling and action setup
+        importMapButton.setOnAction(event -> {
+            try {
+                File importFile = XMLFileOpener.getInstance().open(FileChooseOption.READ);
+                PickyApplication.emptyCityMap();
+                PickyApplication.emptyPlanningRequest(); //New loaded map means we reset the requests too
+                XMLDeserializer.load(PickyApplication.getCityMap(), importFile); //TODO Handle empty city & requests as load file success callback to prevent the "cancel" and invalid format from resetting
+            } catch(Exception e) {
+                e.printStackTrace();
+            } finally {
+                PickyApplication.clearMap();
+                PickyApplication.renderMapAndRequests();
+            }
         });
-    }
 
+        //TODO Move in controller for handling and action setup
+        importPickupButton.setOnAction(event -> {
+            try {
+                File importFile = XMLFileOpener.getInstance().open(FileChooseOption.READ);
+                PickyApplication.emptyPlanningRequest();
+                XMLDeserializer.load(PickyApplication.getPlanningRequest(), PickyApplication.getCityMap(), importFile); //TODO Handle emptyPlanningRequest as load file success callback to prevent the "cancel" and invalid format from resetting
+            } catch(Exception e) {
+                e.printStackTrace();
+            } finally {
+                PickyApplication.clearMap();
+                PickyApplication.renderMapAndRequests();
+            }
+        });
+
+    }
     public Button getImportMapButton() {
         return importMapButton;
     }
@@ -181,5 +107,5 @@ public class ImportView extends Region {
     public Button getComputeButton() {
         return computeButton;
     }
-}
 
+}
