3d33da36402f85b3077738abcace6813705fcf13
bdeleglise <benoit.deleglise@outlook.fr>
2021-10-26 21:37:07 +0200

    save as fonction

--- src/main/java/fr/insalyon/pldagile/xml/XMLSerializer.java
+++ src/main/java/fr/insalyon/pldagile/xml/XMLSerializer.java
@@ -1,27 +1,43 @@
 package fr.insalyon.pldagile.xml;
 
 import com.mitchellbosecke.pebble.PebbleEngine;
+import com.mitchellbosecke.pebble.loader.ClasspathLoader;
+import com.mitchellbosecke.pebble.loader.FileLoader;
+import com.mitchellbosecke.pebble.loader.StringLoader;
 import com.mitchellbosecke.pebble.template.PebbleTemplate;
+import org.apache.log4j.PropertyConfigurator;
 
-import java.io.IOException;
-import java.io.StringWriter;
-import java.io.Writer;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 
 public class XMLSerializer {
-    public static void renderHTMLroadMap() throws IOException {
-        PebbleEngine engine = new PebbleEngine.Builder().build();
+    public static void renderHTMLroadMap() throws IOException, TransformerConfigurationException {
+        File html = HTMLFileOpener.getInstance().open(FileChooseOption.SAVE);
+        FileWriter fstream = new FileWriter(html.getAbsolutePath(), true);
+        BufferedWriter out = new BufferedWriter(fstream);
+
+
+
+        PebbleEngine engine = new PebbleEngine.Builder().loader(new ClasspathLoader()).build();
+
+
         PebbleTemplate compiledTemplate = engine.getTemplate("templates/base.html.peb");
 
         Map<String, Object> context = new HashMap<>();
         context.put("name", "Mitchell");
 
-        /*Writer writer = new StringWriter();
+        Writer writer = new StringWriter();
         compiledTemplate.evaluate(writer, context);
 
         String output = writer.toString();
-        System.out.println("test");
-        System.out.println(output);*/
+        out.write(output);
+        out.close();
+
     }
 }
