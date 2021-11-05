30d659afe200926af849b1ad40cefae4805b4358
Rahim Belateche <rayanebelateche@gmail.com>
2021-11-05 09:11:27 +0100

    Set test

--- src/test/java/fr/insalyon/pldagile/model/CityMapTest.java
+++ src/test/java/fr/insalyon/pldagile/model/CityMapTest.java
@@ -23,7 +23,7 @@ public class CityMapTest {
                 this.expectedResult = expectedResult;
             }
         }
-        TestCase[] tests = {new TestCase(new Coordinates(45.61117752676385,4.977950632220205)),
+        TestCase[] tests = {new TestCase(new Coordinates(45.61117752676385,4.987950632220205)),
         };
         for (TestCase tc : tests) {
             Coordinates actualResult = cityMap.getCenter();
