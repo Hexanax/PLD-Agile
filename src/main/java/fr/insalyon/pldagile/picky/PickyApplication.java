fef08273aec74537b78d4efd916833e4dd90b5d4
Allan Guigal <allan.guigal0@gmail.com>
2021-11-05 12:01:30 +0100

    WIP refactor MVC working city map view

--- src/main/java/fr/insalyon/pldagile/picky/PickyApplication.java
+++ src/main/java/fr/insalyon/pldagile/picky/PickyApplication.java
@@ -1,15 +1,16 @@
 package fr.insalyon.pldagile.picky;
 
 import fr.insalyon.pldagile.controller.Controller;
+import fr.insalyon.pldagile.view.Window;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class Picky extends Application {
-
+public class PickyApplication extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
         Controller controller = new Controller();
+        controller.getWindow().render(stage);
         stage.show();
     }
 
