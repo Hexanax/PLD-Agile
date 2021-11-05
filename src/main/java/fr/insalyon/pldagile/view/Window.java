05be2922fdde16528a7c1ff6ee51e8fe8b45fbb3
Rahim Belateche <rayanebelateche@gmail.com>
2021-11-05 09:03:55 +0100

    fixed client's wanted zoom level

--- src/main/java/fr/insalyon/pldagile/view/Window.java
+++ src/main/java/fr/insalyon/pldagile/view/Window.java
@@ -40,7 +40,7 @@ public class Window {
     private final PointLayer pointLayer = new PointLayer(); // TODO Split point layers in 3 (one city map, one requests,
                                                             // one tour)
     private final LineLayer lineLayer = new LineLayer();
-    private final int centeredZoomValue = 13;
+    private final double centeredZoomValue = 13.5;
 
     public Window(Controller controller) {
         this.controller = controller;
@@ -166,7 +166,7 @@ public class Window {
         MapPoint mapCenter = new MapPoint(coord.getLatitude(), coord.getLongitude());
         // center the map around the calculated center coordinates
         mapView.setCenter(mapCenter);
-        // sets the zoom at level 12: approximately the level of a city in our case
+        // sets the zoom at level 13.5: approximately the level of a city in our case
         mapView.setZoom(centeredZoomValue);
         mapView.setMaxZoomOut(centeredZoomValue);
     }
