6fa7c0e86d5bc9be699ca46119a9897a18bef76c
Allan Guigal <allan.guigal0@gmail.com>
2021-11-08 22:38:35 +0100

    Add synchronized method on click modify

--- src/main/java/fr/insalyon/pldagile/controller/AddRequestState4.java
+++ src/main/java/fr/insalyon/pldagile/controller/AddRequestState4.java
@@ -7,9 +7,14 @@ import fr.insalyon.pldagile.view.Window;
 import javafx.util.Pair;
 
 import java.util.Objects;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class AddRequestState4 implements State {
 
+    private final AtomicBoolean isProcessingRequest = new AtomicBoolean(false);
+    private final ReentrantLock reentrantLock = new ReentrantLock(true);
+    private boolean done = false;
 
     @Override
     public void cancel(Controller controller, PlanningRequest planningRequest, Tour tour, Window window, ListOfCommands l) {
@@ -28,40 +33,47 @@ public class AddRequestState4 implements State {
     }
 
     @Override
-    public void modifyClick(Controller controller, PlanningRequest planningRequest, Tour tour, Long id, String type, int stepIndex, Window window) {
-        validClick = false;
-        if (Objects.equals(type, "Depot") && stepIndex != 0) {
-            window.addWarningStateFollow("You can't add a request after the arrival of the tour");
-        } else {
-            Request request = planningRequest.getLastRequest();
-            if (Objects.equals(id, request.getId()) && Objects.equals(type, "delivery")) {
-                window.addWarningStateFollow("You can't make the request after the request itself");
+    public synchronized void modifyClick(Controller controller, PlanningRequest planningRequest, Tour tour, Long id, String type, int stepIndex, Window window) {
+//        System.out.println("Called modify click");
+//        System.out.println("Is locked = " + reentrantLock.isLocked());
+//        System.out.println("Lock held by current thread = " + reentrantLock.isHeldByCurrentThread());
+//        reentrantLock.lock();
+//        System.out.println("Done = " + done);
+            validClick = false;
+            if (Objects.equals(type, "Depot") && stepIndex != 0) {
+                window.addWarningStateFollow("You can't add a request after the arrival of the tour");
             } else {
-                boolean bufferClicks = false;
-                if (stepIndex == -1) {
-                    bufferClicks = true;
-                    if (type.equals("depot")) {
-                        stepIndex = 0;
-                    } else {
-                        Pair<Long, String> stepToFound = new Pair<Long, String>(id, type);
-                        stepIndex = tour.getSteps().indexOf(stepToFound);
-                    }
-                }
-                if (stepIndex < tour.getSteps().indexOf(new Pair<Long, String>(request.getId(), "pickup"))) {
-                    window.addWarningStateFollow("You can't make the delivery before the pickup");
+                Request request = planningRequest.getLastRequest();
+                if (Objects.equals(id, request.getId()) && Objects.equals(type, "delivery")) {
+                    window.addWarningStateFollow("You can't make the request after the request itself");
                 } else {
-                    tour.addStep(stepIndex, new Pair<>(request.getId(), "delivery"));
-                    Tour modify = new Tour(tour);
-                    controller.setTour(modify);
-                    controller.addRequest();
-                    if (bufferClicks) {
-                        validClick = true;
+                    boolean bufferClicks = false;
+                    if (stepIndex == -1) {
+                        bufferClicks = true;
+                        if (type.equals("depot")) {
+                            stepIndex = 0;
+                        } else {
+                            Pair<Long, String> stepToFound = new Pair<Long, String>(id, type);
+                            stepIndex = tour.getSteps().indexOf(stepToFound);
+                        }
+                    }
+                    if (stepIndex < tour.getSteps().indexOf(new Pair<Long, String>(request.getId(), "pickup"))) {
+                        window.addWarningStateFollow("You can't make the delivery before the pickup");
                     } else {
-                        controller.setCurrentState(controller.addRequestState5);
+                        tour.addStep(stepIndex, new Pair<>(request.getId(), "delivery"));
+                        //Tour modify = new Tour(tour);
+                        //controller.setTour(modify); //CRASH HERE => RequestListView propertyChange => renderOrdered
+                        controller.addRequest();
+                        if (bufferClicks) {
+                            validClick = true;
+                        } else {
+                            controller.setCurrentState(controller.addRequestState5);
+                        }
                     }
                 }
             }
-        }
+//            done = true;
+//            reentrantLock.unlock();
     }
 
     @Override
@@ -71,6 +83,7 @@ public class AddRequestState4 implements State {
             window.mainView();
             window.addStateFollow("Delivery previous address selected, you can now modify the duration in second of the pickup and the delivery or click where you want out of the requests list to valid the creation");
             window.makeLastRequestAddedEditable(true, pclPlanningRequest.getPlanningRequest().getLastRequest().getId());
+            isProcessingRequest.set(false);
         } catch (Exception e) {
             window.addWarningStateFollow(e.getMessage());
             controller.cancel();
